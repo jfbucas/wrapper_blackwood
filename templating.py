e077cd5b7aa72d87379d78071ba64eb7c398323c
Jef Bucas <jfbucas@stp.dias.ie>
2024-11-26 16:25:37 +0000

    client in hostname folder

--- templating.py
+++ templating.py
@@ -1,12 +1,24 @@
 import os
 import sys
 import subprocess
+import socket
 
 import parameters
 
-def gen_template(filename, template_params):
+def get_path():
+	hostname = socket.gethostname()
+	return "solver/"+hostname
 
-	new_file = filename.replace(".template","")
+def gen_template(filename, template_params):
+	# Create a subfolder for that machine
+	path = get_path()
+	if not os.path.exists( path ):
+		try:
+			os.makedirs( path )
+		except OSError as error:
+			print("Couldn't create", path )
+
+	new_file = path+"/"+filename.replace(".template","")
 	if new_file == filename:
 		print(filename, "is not a template")
 		return
@@ -24,11 +36,14 @@ def gen_template(filename, template_params):
 def gen_templates(template_params):
 	gen_template("solver/Program.cs.template", template_params)
 	gen_template("solver/Util.cs.template", template_params)
+	gen_template("solver/Structs.cs.template", template_params)
 
 def compile():
+	path = get_path()
 	#print("Compiling :: cd solver; mcs -unsafe -r:System.Net.Http  Program.cs Util.cs Structs.cs")
-	os.system("cd solver; mcs -unsafe -r:System.Net.Http  Program.cs Util.cs Structs.cs")
+	os.system("cd "+path+"; mcs -unsafe -r:System.Net.Http  Program.cs Util.cs Structs.cs")
  
 def execute():
-	return subprocess.run(["mono", "solver/Program.exe"], stdout=subprocess.PIPE)
+	path = get_path()
+	return subprocess.run(["mono", path+"/Program.exe"], stdout=subprocess.PIPE)
 
